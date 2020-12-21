09aa43daa96962f34aa844c04e2fac5947c721be
Shad Sheikh <shadsheikh3107@gmail.com>
2020-12-21 17:45:48 +0530

    employee

---
+++ lib/p5_Employee.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class p5_employee extends StatefulWidget {
+  @override
+  _p5_employeeState createState() => _p5_employeeState();
+}
+
+class _p5_employeeState extends State<p5_employee> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text (
+          "Employee:",
+        )
+      ),
+    );
+  }
+}
