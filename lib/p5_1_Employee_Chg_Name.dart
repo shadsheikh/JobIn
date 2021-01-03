025c984169911c8ae68ed7783e2c84483d4dba94
Shad Sheikh <shadsheikh3107@gmail.com>
2021-01-03 13:42:38 +0530

    employee_employer_all_file

--- lib/p5_1_Employee_Chg_Name.dart
+++ lib/p5_1_Employee_Chg_Name.dart
@@ -1,3 +1,4 @@
+
 import 'package:flutter/material.dart';
 import 'p5_Employee_Drawer.dart';
 class p5_1_employee_chg_name extends StatefulWidget {
@@ -27,6 +28,47 @@ class _p5_1_employee_chg_nameState extends State<p5_1_employee_chg_name> {
         ],
       ),
         drawer: p5_employee_drawer(),
+
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+
+            Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Padding(
+                        padding: EdgeInsets.only(top: 12.0),
+                        child: Text(
+                          "Current Name",
+                          style: TextStyle(color: Colors.grey),
+                        )),
+                    TextField(
+                      decoration: InputDecoration(
+                        hintText: "Update Name",
+                      ),
+                    )
+                  ],
+                )
+            ),
+
+            RaisedButton(
+              onPressed: (){},
+              child: Text(
+                "Change name",
+                style: TextStyle(
+                  color: Theme.of(context).primaryColor,
+                  fontSize: 20.0,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+
+          ],
+        ),
+      ),
+
     );
   }
 }
--- lib/p5_2_Employee_Job_Appl.dart
+++ lib/p5_2_Employee_Job_Appl.dart
@@ -63,7 +63,180 @@ class _p5_2_employee_job_applState extends State<p5_2_employee_job_appl> {
       ),
       drawer: p5_employee_drawer(),
 
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: container(
+                  context,
+                  'John Marsh',
+                  '20',
+                  'Teaching',
+                  '164, Saket Nagar, Indore, MP',
+                  '9876543210'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: container(
+                  context,
+                  'John Marsh',
+                  '20',
+                  'Teaching',
+                  '164, Saket Nagar, Indore, MP',
+                  '9876543210'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: container(
+                  context,
+                  'John Marsh',
+                  '20',
+                  'Teaching',
+                  '164, Saket Nagar, Indore, MP',
+                  '9876543210'),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: container(
+                  context,
+                  'John Marsh',
+                  '20',
+                  'Teaching',
+                  '164, Saket Nagar, Indore, MP',
+                  '9876543210'),
+            ),
+          ],
+        ),
+      ),
+
     );
   }
 
 }
+
+
+Container container(
+    BuildContext context,
+    String name,
+    String age,
+    String skills,
+    String address,
+    String contact) {
+  var _blue = Colors.blue;
+  var _white = Colors.white;
+  return Container(
+    width: MediaQuery.of(context).size.width * 0.9,
+    child: Tooltip(
+      message: 'Click to View Profile',
+      child: RaisedButton(
+        elevation: 5,
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.all(Radius.circular(20)),
+        ),
+        onPressed: () {},
+        color: _blue,
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RichText(
+                  text: new TextSpan(
+                    style: TextStyle(fontSize: 20.0, color: _white),
+                    children: [
+                      new TextSpan(
+                          text: 'Organization Name : ',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      new TextSpan(
+                        text: '$name',
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RichText(
+                  text: new TextSpan(
+                    style: TextStyle(fontSize: 20.0, color: _white),
+                    children: [
+                      new TextSpan(
+                          text: 'Date of Appolication : ',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      new TextSpan(
+                        text: '$age',
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RichText(
+                  text: new TextSpan(
+                    style: TextStyle(fontSize: 20.0, color: _white),
+                    children: [
+                      new TextSpan(
+                          text: 'Skills Required : ',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      new TextSpan(
+                        text: '$skills',
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RichText(
+                  text: new TextSpan(
+                    style: TextStyle(fontSize: 20.0, color: _white),
+                    children: [
+                      new TextSpan(
+                          text: 'Address : ',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      new TextSpan(
+                        text: '$address',
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: RichText(
+                  text: new TextSpan(
+                    style: TextStyle(fontSize: 20.0, color: _white),
+                    children: [
+                      new TextSpan(
+                          text: 'Contact No. : ',
+                          style: TextStyle(fontWeight: FontWeight.bold)),
+                      new TextSpan(
+                        text: '$contact',
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ),
+  );
+}
+
