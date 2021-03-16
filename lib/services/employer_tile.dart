0358939bbc5a7ec6caf1a72533da050c4104c400
monika9927 <72444713+monika9927@users.noreply.github.com>
2021-03-16 20:50:11 +0000

    Create employer_tile.dart

---
+++ lib/services/employer_tile.dart
@@ -0,0 +1,68 @@
+import 'package:dsc_jobin/Employee_View_Job.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:dsc_jobin/models/employer.dart';
+import 'package:dsc_jobin/Employee_View_Job.dart';
+
+class EmployerTile extends StatelessWidget{
+  final Employer employer;
+  EmployerTile({this.employer});
+
+  @override
+  Widget build(BuildContext context){
+    return Padding(
+        padding: EdgeInsets.only(top: 8.0),
+      child: Card(
+           color: Colors.blue,
+           margin: EdgeInsets.fromLTRB(20.0, 6.0, 20.0, 0.0),
+        child: Column(
+          children: <Widget>[
+            //ListTileTheme(
+              //textColor: Colors.white,
+
+            ListTile(
+              title: Text(
+                  employer.jobtitle,
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+              ),
+              subtitle: Text(
+                  'Vacancy: ${employer.vacancy}',
+
+                style: TextStyle(
+                  color: Colors.white,
+                ),
+               ),
+        ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+               children: <Widget>[
+              TextButton(
+                child: const Text('...more'),
+                style: TextButton.styleFrom(
+                  primary: Colors.white,
+                ),
+                onPressed: ()
+                  {
+
+                    Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => EmployeeJob(),
+                          settings: RouteSettings(
+                            arguments: this.employer,
+                          )
+                        ));
+
+                  },
+               ),
+                 const SizedBox(width: 8),
+             ]
+            )
+        ]
+        )
+      )
+    );
+  }
+}
