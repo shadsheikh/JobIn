d2035a473f69d6c0086d9b5a5a3cd1d0a5f98a34
monika9927 <72444713+monika9927@users.noreply.github.com>
2021-03-16 20:46:23 +0000

    Create employer.dart

---
+++ lib/models/employer.dart
@@ -0,0 +1,16 @@
+class Employer{
+  final String city;
+  final String jobtitle;
+  final String salary;
+  final String jobmode;
+  final String jobtype;
+  final String vacancy;
+  final String skills;
+  final String state;
+  final String user_uid;
+
+
+  Employer({this.jobtitle,this.salary,this.vacancy,this.skills,this.jobtype,this.jobmode,this.state,this.city,this.user_uid});
+
+
+}
