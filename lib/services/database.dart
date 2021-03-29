a1e241ef2861fdaa9e36f355c01334384c4d4e81
monika9927 <72444713+monika9927@users.noreply.github.com>
2021-03-29 17:30:14 +0530

    Update database.dart

--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,6 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:dsc_jobin/models/employer.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:dsc_jobin/models/employee.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -15,7 +16,32 @@ class DatabaseService{
 
 
   final CollectionReference employerCollection = FirebaseFirestore.instance.collection('JOB');
+  final CollectionReference employeeJob = FirebaseFirestore.instance
+      .collection("Employee")
+      .doc(FirebaseAuth.instance.currentUser.uid)
+      .collection("JOB_Applied");
 
+  //employee Job applied list from snapshot
+  List<Employee> _employeeListFromSnapshot(QuerySnapshot snapshot){
+    return snapshot.docs.map((doc){
+      return Employee(
+        city: doc.data()['city'].toString() ,
+        jobtitle: doc.data()['jobtittle'].toString() ,
+        salary: doc.data()['salary'].toString() ,
+        jobmode: doc.data()['jobmode'].toString() ,
+        jobtype: doc.data()['jobtype'].toString() ,
+        vacancy: doc.data()['vaccancy'].toString() ,
+        skills: doc.data()['skills'].toString() ,
+        state: doc.data()['state'].toString() ,
+        uid: doc.data()['uid'].toString(),
+      );
+    }).toList();
+  }
+
+  Stream<List<Employee>> get employee{
+    return employeeJob.snapshots()
+        .map(_employeeListFromSnapshot);
+  }
 
   //employer list from snapshot
   List<Employer> _employerListFromSnapshot(QuerySnapshot snapshot){
