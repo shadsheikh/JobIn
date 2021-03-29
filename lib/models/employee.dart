c30bcaeee438837bd8db5c5a3f729356935c3989
monika9927 <72444713+monika9927@users.noreply.github.com>
2021-03-29 17:28:37 +0530

    Create employee.dart

---
+++ lib/models/employee.dart
@@ -0,0 +1,16 @@
+class Employee{
+  final String city;
+  final String jobtitle;
+  final String salary;
+  final String jobmode;
+  final String jobtype;
+  final String vacancy;
+  final String skills;
+  final String state;
+  final String uid;
+
+
+  Employee({this.jobtitle,this.salary,this.vacancy,this.skills,this.jobtype,this.jobmode,this.state,this.city,this.uid});
+
+
+}
